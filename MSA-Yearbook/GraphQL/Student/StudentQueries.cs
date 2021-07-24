cf1909a48a088c9cd50348ed3f8f526c08637b92
Zach <wangziqi0325@gmail.com>
2021-07-24 14:11:31 +1200

    init

---
+++ MSA-Yearbook/GraphQL/Student/StudentQueries.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using HotChocolate;
+using MSAYearbook.Data;
+using MSAYearbook.Models;
+
+namespace MSAYearbook.GraphQL.Students
+{
+    [HotChocolate.Types.ExtendObjectType(name: "Query")]
+    public class StudentQueries
+    {
+        public IQueryable<Student> GetStudents([ScopedService] AppDbContext context)
+        {
+            return context.Students;
+        }
+    }
+}
